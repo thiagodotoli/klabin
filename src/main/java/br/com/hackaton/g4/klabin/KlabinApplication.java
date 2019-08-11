6676fd957c1ca96e1dcf4cbdd5a0533dd8828bc7
Thiago Bento <thiagodb@hotmail.com>
2019-08-11 10:09:37 -0300

    first commit

---
+++ src/main/java/br/com/hackaton/g4/klabin/KlabinApplication.java
@@ -0,0 +1,13 @@
+package br.com.hackaton.g4.klabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlabinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlabinApplication.class, args);
+	}
+
+}
